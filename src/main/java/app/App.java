0da889fb0bff6bcb31fe1e900217ffd3ed0a959f
Leonie Theobald <leonie@fake-mail.com>
2024-07-04 16:21:52 +0200

    Include time measurement, analysis, and logging

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -29,7 +29,8 @@ public class App {
     private static final Logger LOGGER = LogManager.getLogger();
 
     public static void main(String[] args) {
-        Config myConfig =
+        /*
+        Config myConfigEllipticCurve =
             ConfigFactory.getConfig(
                 TlsVersion.TLS12,
                 KeyExchange.ECDHE,
@@ -40,141 +41,30 @@ public class App {
         OutboundConnection outboundCon = new OutboundConnection();
         outboundCon.setHostname("localhost");
         outboundCon.setPort(4433);
+        myConfigEllipticCurve.setDefaultClientConnection(outboundCon);
+        
+        List<WorkflowTrace> segmentedHandshake = HandshakeStepping.getSegmentedHandshake(HandshakeType.TLS12_EPHEMERAL_WITHOUT_CLIENT_AUTH, myConfigEllipticCurve, outboundCon);
+        */
+
+        Config myConfig =
+            ConfigFactory.getConfig(
+                TlsVersion.TLS12,
+                KeyExchange.RSA,
+                SignatureScheme.RSA_SHA384,
+                BulkAlgo.AES_256_GCM,
+                new Vector<Extension>());
+
+        OutboundConnection outboundCon = new OutboundConnection();
+        outboundCon.setHostname("localhost");
+        outboundCon.setPort(4433);
         myConfig.setDefaultClientConnection(outboundCon);
         
-        List<WorkflowTrace> segmentedHandshake = HandshakeStepping.getSegmentedHandshake(HandshakeType.TLS12_EPHEMERAL_WITHOUT_CLIENT_AUTH, myConfig, outboundCon);
+        List<WorkflowTrace> segmentedHandshake = HandshakeStepping.getSegmentedHandshake(HandshakeType.TLS12_STATIC_WITHOUT_CLIENT_AUTH, myConfig, outboundCon);
 
-        ArrayList<ArrayList<Long>> resultsMeasurement = TimeMeasurement.startTimeMeasurement(200, myConfig, segmentedHandshake);
-        //System.out.println("Results time measurement: " + resultsMeasurement);
-
-        List<LongSummaryStatistics> analysisResults = TimeMeasurement.runStatisticAnalysis(resultsMeasurement);
-        //System.out.println("Results time analysis: " + analysisResults);
-        TimeMeasurement.printStatisticAnalysisResults(analysisResults);
-        //App.startTlsClient(myConfig, segmentedHandshake.get(0));
+        String resultsMeasurement = TimeMeasurement.startTimeMeasurement(2, myConfig, segmentedHandshake, true);
+        System.out.println(resultsMeasurement);
 
         System.out.println("Reached End");
-
-        /*
-        File configFileVar1 = new File("/Users/lth/Library/Mobile Documents/com~apple~CloudDocs/Zweitstudium/Module/00_Masterarbeit/Netzwerk/Bearbeitung/TLS-Attacker/TLS-Attacker/Zusatzzeug/tls12_short_forced-cipher_sig-ECDSA-SHA224.config");
-        Config var1Config = Config.createConfig(configFileVar1);
-        OutboundConnection outboundConnectionVar1 = new OutboundConnection();
-        outboundConnectionVar1.setHostname("localhost");
-        outboundConnectionVar1.setPort(123);
-        var1Config.setDefaultClientConnection(outboundConnectionVar1);
-        WorkflowTrace workflowTraceVar1 = new WorkflowConfigurationFactory(var1Config).createWorkflowTrace(var1Config.getWorkflowTraceType(), RunningModeType.CLIENT);
-
-        File configFileVar2 = new File("/Users/lth/Library/Mobile Documents/com~apple~CloudDocs/Zweitstudium/Module/00_Masterarbeit/Netzwerk/Bearbeitung/TLS-Attacker/TLS-Attacker/Zusatzzeug/tls12_short_forced-cipher_sig-ECDSA-SHA256.config");
-        Config var2Config = Config.createConfig(configFileVar2);
-        OutboundConnection outboundConnectionVar2 = new OutboundConnection();
-        outboundConnectionVar2.setHostname("localhost");
-        outboundConnectionVar2.setPort(456);
-        var2Config.setDefaultClientConnection(outboundConnectionVar2);
-        WorkflowTrace workflowTraceVar2 = new WorkflowConfigurationFactory(var2Config).createWorkflowTrace(var2Config.getWorkflowTraceType(), RunningModeType.CLIENT);
-
-        File configFileVar3 = new File("/Users/lth/Library/Mobile Documents/com~apple~CloudDocs/Zweitstudium/Module/00_Masterarbeit/Netzwerk/Bearbeitung/TLS-Attacker/TLS-Attacker/Zusatzzeug/tls12_short_forced-cipher_sig-ECDSA-SHA512.config");
-        Config var3Config = Config.createConfig(configFileVar3);
-        OutboundConnection outboundConnectionVar3 = new OutboundConnection();
-        outboundConnectionVar3.setHostname("localhost");
-        outboundConnectionVar3.setPort(789);
-        var3Config.setDefaultClientConnection(outboundConnectionVar3);
-        WorkflowTrace workflowTraceVar3 = new WorkflowConfigurationFactory(var3Config).createWorkflowTrace(var3Config.getWorkflowTraceType(), RunningModeType.CLIENT);
-
-        int count = 200;
-
-        long[] arrayVar1 = new long[count];
-        for (int i = 0; i < count; i++) {
-            long timeElapsedVar1 = App.startTlsClient(var1Config, workflowTraceVar1);
-            arrayVar1[i] = timeElapsedVar1;
-            //System.out.println(i + ": " + arrayVar1[i]);
-        }
-        long minVar1 = 999999999;
-        long maxVar1 = 0;
-        long sumVar1 = 0;
-        for (int i = 0; i < arrayVar1.length; i++) {
-            sumVar1 += arrayVar1[i];
-            if (arrayVar1[i] > maxVar1) {
-                maxVar1 = arrayVar1[i];
-            }
-            if (arrayVar1[i] < minVar1) {
-                minVar1 = arrayVar1[i];
-            }
-        }
-        long averageVar1 = sumVar1 / arrayVar1.length;
-
-        long[] arrayVar2 = new long[count];
-        for (int i = 0; i < count; i++) {
-            long timeElapsedVar2 = App.startTlsClient(var2Config, workflowTraceVar2);
-            arrayVar2[i] = timeElapsedVar2;
-            //System.out.println(i + ": " + arrayVar2[i]);
-        }
-        long minVar2 = 999999999;
-        long maxVar2 = 0;
-        long sumVar2 = 0;
-        for (int i = 0; i < arrayVar2.length; i++) {
-            sumVar2 += arrayVar2[i];
-            if (arrayVar2[i] > maxVar2) {
-                maxVar2 = arrayVar2[i];
-            }
-            if (arrayVar2[i] < minVar2) {
-                minVar2 = arrayVar2[i];
-            }
-        }
-        long averageVar2 = sumVar2 / arrayVar2.length;
-
-        long[] arrayVar3 = new long[count];
-        for (int i = 0; i < count; i++) {
-            long timeElapsed = App.startTlsClient(var3Config, workflowTraceVar3);
-            arrayVar3[i] = timeElapsed;
-            //System.out.println(i + ": " + arrayVar3[i]);
-        }
-        long minVar3 = 999999999;
-        long maxVar3 = 0;
-        long sumVar3 = 0;
-        for (int i = 0; i < arrayVar3.length; i++) {
-            sumVar3 += arrayVar3[i];
-            if (arrayVar3[i] > maxVar3) {
-                maxVar3 = arrayVar3[i];
-            }
-            if (arrayVar3[i] < minVar3) {
-                minVar3 = arrayVar3[i];
-            }
-        }
-        long averageVar3 = sumVar3 / arrayVar3.length;
-
-
-        System.out.println("\n");
-        System.out.println("Var1 average is: " + averageVar1/1000000.0 + " ms");
-        System.out.println("Var1 min is: " + minVar1/1000000.0 + " ms");
-        System.out.println("Var1 max is: " + maxVar1/1000000.0 + " ms");
-
-        System.out.println("\n");
-        System.out.println("Var2 average is: " + averageVar2/1000000.0 + " ms");
-        System.out.println("Var2 min is: " + minVar2/1000000.0 + " ms");
-        System.out.println("Var2 max is: " + maxVar2/1000000.0 + " ms");
-
-        System.out.println("\n");
-        System.out.println("Var3 average is: " + averageVar3/1000000.0 + " ms");
-        System.out.println("Var3 min is: " + minVar3/1000000.0 + " ms");
-        System.out.println("Var3 max is: " + maxVar3/1000000.0 + " ms");
-
-        */
-
-        /* NOTE on results
-           File configFileVar1 = new File("/Users/lth/Library/Mobile Documents/com~apple~CloudDocs/Zweitstudium/Module/00_Masterarbeit/Netzwerk/Bearbeitung/TLS-Attacker/TLS-Attacker/Zusatzzeug/tls12_short_forced-cipher_sig-ECDSA-SHA224.config");
-           Var1 average is: 4.482707 ms
-           Var1 min is: 3.935625 ms
-           Var1 max is: 118.931708 ms
-
-           File configFileVar2 = new File("/Users/lth/Library/Mobile Documents/com~apple~CloudDocs/Zweitstudium/Module/00_Masterarbeit/Netzwerk/Bearbeitung/TLS-Attacker/TLS-Attacker/Zusatzzeug/tls12_short_forced-cipher_sig-ECDSA-SHA256.config");
-           Var2 average is: 4.3037 ms
-           Var2 min is: 3.866917 ms
-           Var2 max is: 198.390584 ms
-
-           File configFileVar3 = new File("/Users/lth/Library/Mobile Documents/com~apple~CloudDocs/Zweitstudium/Module/00_Masterarbeit/Netzwerk/Bearbeitung/TLS-Attacker/TLS-Attacker/Zusatzzeug/tls12_short_forced-cipher_sig-ECDSA-SHA512.config");
-           Var3 average is: 4.147134 ms
-           Var3 min is: 3.759292 ms
-           Var3 max is: 11.553083 ms
-        */
     }
 
     public static long startTlsClient(Config config, WorkflowTrace trace) {
--- src/main/java/app/TimeMeasurement.java
+++ src/main/java/app/TimeMeasurement.java
@@ -1,17 +1,30 @@
 package app;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.LongSummaryStatistics;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Vector;
+
+import com.google.common.io.Files;
 
 import app.ConfigurationTypes.BulkAlgo;
 import app.ConfigurationTypes.Extension;
 import app.ConfigurationTypes.KeyExchange;
 import app.ConfigurationTypes.SignatureScheme;
 import app.ConfigurationTypes.TlsVersion;
+import app.HandshakeStepping.HandshakeType;
 import de.rub.nds.tlsattacker.core.config.Config;
 import de.rub.nds.tlsattacker.core.connection.AliasedConnection;
 import de.rub.nds.tlsattacker.core.protocol.ProtocolMessage;
@@ -37,12 +50,15 @@ import de.rub.nds.tlsattacker.core.workflow.action.SendDynamicServerCertificateA
 import de.rub.nds.tlsattacker.core.workflow.action.SendDynamicServerKeyExchangeAction;
 import de.rub.nds.tlsattacker.transport.ConnectionEndType;
 
-public class TimeMeasurement {    
-    public static ArrayList<ArrayList<Long>> startTimeMeasurement(
+public class TimeMeasurement {
+    // perform measurement for one config and one segmented handshake collection
+    public static String startTimeMeasurement(
         int repetition,
         Config config,
-        List<WorkflowTrace> segmentedHandshake
+        List<WorkflowTrace> segmentedHandshake,
+        Boolean shouldDocument
     ) {
+        // run repeatedly through handshake segments
         ArrayList<ArrayList<Long>> durationForHandshakeSegments = new ArrayList<ArrayList<Long>>();
             
         int segCnt = 0;
@@ -56,9 +72,19 @@ public class TimeMeasurement {
             segCnt++;
         }
 
-        return durationForHandshakeSegments;
+        // Run Statistic Analysis and create report
+        ArrayList<LongSummaryStatistics> analysisResults = runStatisticAnalysis(durationForHandshakeSegments);
+        String analysisOverview = giveStatisticAnalysisResults(analysisResults);
+
+        // log results if wished
+        if (shouldDocument == true) {
+            logMeasurement(config, segmentedHandshake, durationForHandshakeSegments, analysisResults);
+        }
+
+        return analysisOverview;
     }
 
+    // performs statistical analysis for each handshake segment including all the repititons
     public static ArrayList<LongSummaryStatistics> runStatisticAnalysis(ArrayList<ArrayList<Long>> durationForHandshakeSegments) {
         ArrayList<LongSummaryStatistics> analysisList = new ArrayList<LongSummaryStatistics>();
 
@@ -70,22 +96,88 @@ public class TimeMeasurement {
         return analysisList;
     }
 
-    public static void printStatisticAnalysisResults(List<LongSummaryStatistics> analysisList) {
+    // creates text overview of statistical analysis
+    public static String giveStatisticAnalysisResults(List<LongSummaryStatistics> analysisList) {
         int cnt = 0;
-
-        System.out.println("\n");
-        System.out.println("Results over " + analysisList.get(0).getCount() + " repetitions for each handshake segment.");
-        System.out.println("\n");
+        String analysisResultsString = new String();
+        analysisResultsString += "\n\nResults over " + analysisList.get(0).getCount() + " repetitions for each handshake segment.\n";
 
         for (LongSummaryStatistics lss: analysisList) {
-            System.out.println("Handshake Segment " + cnt);
+            analysisResultsString += "Handshake Segment " + cnt + "\n";
+            analysisResultsString += cnt + " Min: " + lss.getMin()/1000000.0 + " ms\n";
+            analysisResultsString += cnt + " Max: " + lss.getMax()/1000000.0 + " ms\n";
+            analysisResultsString += cnt + " Average: " + lss.getAverage()/1000000.0 + " ms\n\n";
 
-            System.out.println(cnt + " Min: " + lss.getMin()/1000000.0 + " ms");
-            System.out.println(cnt + " Max: " + lss.getMax()/1000000.0 + " ms");
-            System.out.println(cnt + " Average: " + lss.getAverage()/1000000.0 + " ms");  
-            System.out.println("\n");
-            
             cnt++;
         }
+
+        return analysisResultsString;
     }
+
+    // logs raw data and statistical analysis results into file
+    public static void logMeasurement(
+        Config config,
+        List<WorkflowTrace> segmentedHandshake,
+        ArrayList<ArrayList<Long>> durationForHandshakeSegments,
+        List<LongSummaryStatistics> analysisList
+    ) {
+        try {
+            // Get path of the JAR file and strip unnecessary folders
+            String jarPath = App.class
+                    .getProtectionDomain()
+                    .getCodeSource()
+                    .getLocation()
+                    .toURI()
+                    .getPath();
+            String basePath = jarPath.substring(0, jarPath.lastIndexOf("target"));
+            
+            Date now = Calendar.getInstance().getTime();
+            String nowAsString = new SimpleDateFormat("yyyy-mm-dd_HH-mm-ss-mmmm").format(now);
+            String logFileName = nowAsString + "_measurement-results";
+
+            File logFile = new File(basePath + "logging/" + logFileName);
+
+            try (PrintWriter out = new PrintWriter(logFile)) {
+                out.println("TIME MEASUREMENT RESULTS\n" + nowAsString);
+                
+                out.println("\n\n#################################");
+                out.println("Used Configuration\n");
+                out.print(getConfigOverview(config));
+
+                out.println("\n\n#################################");
+                out.println("Used Handshake Segments\n");
+                out.print(segmentedHandshake);
+
+                out.println("\n\n#################################");
+                out.println("Used Repititions\n");
+                out.print(analysisList.get(0).getCount());
+                
+                out.println("\n\n#################################");
+                out.println("Anaylsis Results");
+                out.print(giveStatisticAnalysisResults(analysisList));
+                
+                out.println("\n\n##################################################################");
+                out.println("Detailled Measurement Results");
+                out.print(durationForHandshakeSegments);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getConfigOverview(Config config) {
+        String configDescription = new String();
+
+        configDescription = "Config\n";
+        configDescription += "\nHighest TLS Version: " + config.getHighestProtocolVersion();
+        configDescription += "\nDefault Selected Cipher Suite: " + config.getDefaultClientConnection();
+
+        return configDescription;
+    }
+
 }
